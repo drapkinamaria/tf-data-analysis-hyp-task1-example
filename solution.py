ec7ede5c80141747223099fb39dd61a976ba1818
drapkinamaria <drapkinamaria02@gmail.com>
2023-04-14 01:44:05 +0500

    try3

--- solution.py
+++ solution.py
@@ -11,4 +11,4 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return proportions_ztest([y_success, x_success], [y_cnt, x_cnt], alternative="larger")[1] < 0.01 # Ваш ответ, True или False
+    return proportions_ztest([y_success, x_success], [y_cnt, x_cnt], alternative="smaller")[1] <= 0.01 # Ваш ответ, True или False
